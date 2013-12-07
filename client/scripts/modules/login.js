aa31e71b0ffcfafcfbc64edd31005839f42a2eba
Timm Allman <tallman@umass.edu>
2013-12-07 15:12:38 -0500

    Add more templates

--- client/scripts/modules/login.js
+++ client/scripts/modules/login.js
@@ -1,3 +1,11 @@
-define( function () {
-	return {};
+define([ '$', 'Backbone', '_', 'gameLevel', 'store', 'newsfeed', 'leaderboard' ], function() {
+	var Login = Backbone.Model.extend( {
+		initialize : function() {
+
+		}
+	} );
+
+	return {
+		login : Login
+	};
 } );
