4836415dbb7d50a2f6f24a0e134989eba16c6edf
Henry Merriam <hmerriam@umass.edu>
2013-12-10 22:02:28 -0500

    Sorting

--- client/scripts/leaderboard.js
+++ client/scripts/leaderboard.js
@@ -25,6 +25,9 @@ define([ 'knockout', 'modules/communication' ], function(ko, communication) {
 			for (friend in list) {
 				self.friendEntries.push(new EntryViewModel(friend, list[friend]));
 			}
+			self.friendEntries.sort(function(left, right) {
+				return left.score - right.score;
+			});
 		});	
 	}
 
